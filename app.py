b967ed4c6969744f95fba43ebc9db70cb362dce2
Lía Da Silva Rojas <39600651+programat3@users.noreply.github.com>
2025-03-07 17:59:44 -0300

    Update app.py PyCon US

--- app.py
+++ app.py
@@ -44,12 +44,12 @@ def about(medicamento):
     generation_config=generation_config,
     )
     pdf_gemini = genai.upload_file(pdf_io, mime_type='application/pdf')
-    prompt = f"si no hay registros en la tabla, responde el número 0 separado por una coma otro 0. Si hay registros en la tabla, responde el número 1 separado por una coma de la fecha más nueva existente en la tabla, en formato DD-MM-AAAA."
+    prompt = f"Recibes un PDF que contiene una tabla, si no hay registros en la tabla, responde el número 0 y, separado por una coma otro 0. Si hay registros en la tabla, responde el número 1 separado por una coma de la fecha más nueva existente en la tabla en formato DD-MM-AAAA."
     response = model.generate_content([prompt, pdf_gemini])
-    response_number, date = response.text.split(',')
-    response_number = int(response_number)
+    numero_respuesta, fecha = response.text.split(',')
+    numero_respuesta = int(numero_respuesta)
 
-    if response_number == 1:
+    if numero_respuesta == 1:
         temp_pdf = tempfile.NamedTemporaryFile(delete=False, suffix=".pdf")
         try:
             temp_pdf.write(pdf_m)
@@ -66,27 +66,28 @@ def about(medicamento):
             gc.collect()
             os.remove(temp_pdf_path)
         urls = enlaces
-        dia,mes,agno = date.split('-')
+        dia,mes,agno = fecha.split('-')
         alerta_url = ""
         for url in urls['pdf']:
             if  mes.strip() in str(url) and agno.strip() in str(url): 
                 alerta_url = url
                 break
-        response_alerta = requests.get(alerta_url,verify=False)
-        if response_alerta.status_code == 200:
+        respuesta_alerta = requests.get(alerta_url,verify=False)
+        if respuesta_alerta.status_code == 200:
             pdf_alerta = response_alerta.content
             pdf_alerta_io = io.BytesIO(bytes(pdf_alerta))
             pdf_gemini_alerta = genai.upload_file(pdf_alerta_io, mime_type='application/pdf')
             prompt_alerta = f"Según el pdf adjunto qué números de serie o lotes tienen alertas o han tenido alertas en el pasado, entrega sólo los números separados por comas"
-            response_alerta_lotes = model.generate_content([prompt_alerta, pdf_gemini_alerta])
-            prompt_razones = f"según el pdf adjunto, qué razones se dan para la alerta, si es que hay alguna, en español"
-            response_alerta_razones = model.generate_content([prompt_razones, pdf_gemini_alerta])
+            respuesta_alerta_lotes = model.generate_content([prompt_alerta, pdf_gemini_alerta])
             lotes = response_alerta_lotes.text.split(',')
+            
+            prompt_razones = f"Según el pdf adjunto, qué razones se dan para la alerta, si es que hay alguna"
+            respuesta_alerta_razones = model.generate_content([prompt_razones, pdf_gemini_alerta])
             razones = response_alerta_razones.text
             alerta = True
-            fecha = f'{dia} del {mes} del {agno}'
+            fecha_s = f'{dia} del {mes} del {agno}'
     else:  
        alerta = False
-    return render_template('index.html',alerta=alerta,medicamento=med,lotes=lotes,razones=razones, fecha=fecha)
+    return render_template('index.html',alerta=alerta,medicamento=med,lotes=lotes,razones=razones, fecha=fecha_s)
 
     
